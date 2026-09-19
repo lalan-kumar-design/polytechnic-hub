3127de57ccadc2cdaa973d0a1bd8013a2d29747c
lalan-kumar-design <lk9973532153@gmail.com>
2026-09-19 21:32:53 +0530

    Add new engineering branches to courses page

--- app/courses/page.js
+++ app/courses/page.js
@@ -28,6 +28,53 @@ export default function Courses() {
               <p>6 Subjects</p>
             </div>
           </div>
+           <div className="branch-card">
+  <img
+    src="/images/electrical.webp"
+    alt="Electrical Engineering"
+  />
+
+  <div className="branch-info">
+    <h2>Electrical Engineering</h2>
+    <p>6 Subjects</p>
+  </div>
+</div>
+
+<div className="branch-card">
+  <img
+    src="/images/mechanical.webp"
+    alt="Mechanical Engineering"
+  />
+
+  <div className="branch-info">
+    <h2>Mechanical Engineering</h2>
+    <p>6 Subjects</p>
+  </div>
+</div>
+
+<div className="branch-card">
+  <img
+    src="/images/computer-science.png"
+    alt="Computer Science Engineering"
+  />
+
+  <div className="branch-info">
+    <h2>Computer Science Engineering</h2>
+    <p>6 Subjects</p>
+  </div>
+</div>
+
+<div className="branch-card">
+  <img
+    src="/images/electronics.png"
+    alt="Electronics Engineering"
+  />
+
+  <div className="branch-info">
+    <h2>Electronics Engineering</h2>
+    <p>6 Subjects</p>
+  </div>
+</div>
 
         </section>
 
