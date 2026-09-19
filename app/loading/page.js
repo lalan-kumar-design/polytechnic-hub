cc1a7ff8becc7f8ee4992b50008de2faecd4c442
lalan-kumar-design <lk9973532153@gmail.com>
2026-09-19 21:38:21 +0530

    Create LoadingPage component for loading state

---
+++ app/loading/page.js
@@ -0,0 +1,15 @@
+export default function LoadingPage() {
+  return (
+    <main className="loading-page">
+      <div className="loading-box">
+        <div className="loading-spinner"></div>
+
+        <h1>Loading...</h1>
+
+        <p>
+          Preparing your learning experience
+        </p>
+      </div>
+    </main>
+  );
+    }
